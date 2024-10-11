9fadd0594a0e1691d907367e8ad10ff89d844225
SufiyarovDanil <danil-sufiyarov@mail.ru>
2024-10-11 18:46:30 +0500

    перевод многоканальных аудио в моноканальные

--- app/src/data_extraction.py
+++ app/src/data_extraction.py
@@ -1,9 +1,7 @@
 from enum import StrEnum
 from typing import BinaryIO
 
-# import librosa
-# from librosa.feature import mfcc
-from torch import Tensor
+from torch import mean
 from torchaudio import load
 from torchaudio.functional import resample
 from torchaudio.transforms import MFCC
@@ -17,24 +15,35 @@ class AudioResult(StrEnum):
 
 async def determine_audio_auth(audio_file: BinaryIO) -> AudioResult:
     sample_rate: int = 1024 * 6
-    sample, sr = load(audio_file, normalize=True)
+    two_secs: int = 2
+    sample, sr = load(audio_file, normalize=True, channels_first=True)
+    max_audio_duration: int = sr * two_secs
+
+    if len(sample[0]) < max_audio_duration:
+        raise Exception('Audio duration is less than two seconds')
+
+    sample = sample[:, 0:sr * two_secs]
+    sample = mean(sample, dim=0, keepdim=True)
+    print('sample shape:', sample.shape)
+
     resampled = resample(sample, sr, sample_rate)
 
     transform = MFCC(sample_rate=sample_rate, n_mfcc=40)
-    mfcc_1 = transform(resampled[0].unsqueeze(0)).numpy()
-    mfcc_2 = transform(resampled[1].unsqueeze(0)).numpy()
-    onnx_session = InferenceSession('./res/model.onnx')
+    mfcc = transform(resampled.unsqueeze(0)).numpy()
+
+    print('mfcc shape:', mfcc.shape)
+
+    onnx_session = InferenceSession('./res/test.onnx')
 
     try:
-        output_1 = onnx_session.run(['sigmoid_1'], {'l_x_': [mfcc_1]})
-        output_2 = onnx_session.run(['sigmoid_1'], {'l_x_': [mfcc_2]})
+        output = onnx_session.run(['sigmoid_1'], {'l_x_': mfcc})
     except Exception as e:
         print(e)
         return AudioResult.fake
 
-    sigmoid_val_avg: float = (output_1[0] + output_2[0]) / 2.0
+    sigmoid_val: float = float(output[0])
 
-    if sigmoid_val_avg > 0.5:
+    if sigmoid_val > 0.5:
         return AudioResult.fake
 
     return AudioResult.real
